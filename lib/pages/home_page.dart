b93e0505a8157021b43e0a440b94fc947e15111b
jenn-jk <jenkins051@gmail.com>
2025-08-02 20:39:38 +0530

    refactored grid view widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
+import 'package:books_tracker/components/grid_view_widget.dart';
 import 'package:books_tracker/models/book.dart';
 import 'package:books_tracker/network.dart';
-import 'package:books_tracker/utils/book_details_arguments.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +11,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final String _query = "The Shining";
   List<Book> _books = [];
   Network network = Network();
 
@@ -46,68 +45,11 @@ class _HomePageState extends State<HomePage> {
                 onSubmitted: (query) => _searchBooks(query),
               ),
             ),
-            Expanded(
-              child: GridView.builder(
-                itemCount: _books.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 0.6,
-                ),
-                itemBuilder: (context, index) {
-                  Book book = _books[index];
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.surfaceVariant,
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                    ),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(
-                          context,
-                          "/bookdetails",
-                          arguments: BookDetailsArguments(itemBook: book),
-                        );
-                      },
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(6.0),
-                            child: Image.network(
-                              book.imageLinks["thumbnail"]?.replaceFirst(
-                                    "http://",
-                                    "https://",
-                                  ) ??
-                                  "",
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              book.title,
-                              style: Theme.of(context).textTheme.titleSmall,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              book.authors.join(", "),
-                              style: Theme.of(context).textTheme.bodySmall,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
+            GridViewWidget(books: _books),
           ],
         ),
       ),
     );
   }
 }
+
